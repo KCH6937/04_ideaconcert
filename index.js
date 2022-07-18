845bd9dfb34952732e1af2214ad701193ded7113
Woody-Park <starface1009@gmail.com>
2022-07-18 10:02:02 +0900

    [Fix] hello world

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("api/hello", (req, res) => {
+app.get("/api/hello", (req, res) => {
   res.send("hello world");
 });
 
